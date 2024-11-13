6e1272c9fed31d4af59fc90445625b1bc5782d2d
Samuel Ramos <samuelaoliveiraramos@gmail.com>
2024-11-13 19:39:57 -0300

    export additional components: Footer, Navbar, Search, and Text

--- src/shared/components/index.js
+++ src/shared/components/index.js
@@ -1,4 +1,8 @@
 // TODO: Export all components from this file
 
 export * from "./ButtonLink";
+export * from "./Footer";
+export * from "./Navbar";
+export * from "./Search";
 export * from "./Stack";
+export * from "./Text";
